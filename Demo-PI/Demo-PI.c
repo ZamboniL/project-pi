c0a09b772521677edc014165d6af181fbbe09956
Maria Coelho <maeriacoelho@gmail.com>
2023-10-11 15:38:05 -0300

    Criação de quadrados com detecção

--- Demo-PI/Demo-PI.c
+++ Demo-PI/Demo-PI.c
@@ -16,6 +16,17 @@
 #define TURN_BOX_Y 360
 #define TURN_BOX_RADIUS 30
 
+float square1_x = 450.0;
+float square1_y = 100.0;
+float square2_x = 100.0;
+float square2_y = 100.0;
+// quadrado verde
+float square1_width = 100.0;
+float square1_height = 100.0;
+// quadrado azul
+float square2_width = 100.0;
+float square2_height = 100.0;
+
 
 bool is_mouse_over_turn_box(int mx, int my) {
     int box_max_x = (TURN_BOX_X + TURN_BOX_RADIUS);
@@ -80,6 +91,7 @@ int main()
                         turn++;
                     }
                 }
+
         }
 
         char turn_text[10];
@@ -110,6 +122,35 @@ int main()
 
             al_draw_text(font, al_map_rgb(255, 255, 255), TURN_BOX_X + 0, TURN_BOX_Y - 3, ALLEGRO_ALIGN_CENTER, turn_text);
             al_draw_circle(TURN_BOX_X, TURN_BOX_Y, TURN_BOX_RADIUS, al_map_rgb_f(1, 0, 1), 2);
+            al_draw_filled_rectangle(square1_x, square1_y, square1_x + 100, square1_y + 100, al_map_rgb(0, 255, 0));
+            al_draw_filled_rectangle(square2_x, square2_y, square2_x + 100, square2_y + 100, al_map_rgb(0, 0, 255));
+            ALLEGRO_COLOR box_color_green = al_map_rgb(0, 255, 0);  
+            ALLEGRO_COLOR box_color_blue = al_map_rgb(0, 0, 255);   
+            ALLEGRO_COLOR highlight_color = al_map_rgb(255, 255, 0); // Cor de destaque quando o mouse está dentro
+            int mouse_x = event.mouse.x;
+            int mouse_y = event.mouse.y;
+            // Verifique a primeira caixa (quadrado verde)
+            if (mouse_x >= square1_x && mouse_x <= square1_x + square1_width &&
+                mouse_y >= square1_y && mouse_y <= square1_y + square1_height) {
+                // O mouse está dentro da primeira caixa (quadrado verde)
+                al_draw_filled_rectangle(square1_x, square1_y, square1_x + square1_width, square1_y + square1_height, highlight_color);
+            }
+            else {
+                // O mouse não está dentro da primeira caixa (quadrado verde)
+                al_draw_filled_rectangle(square1_x, square1_y, square1_x + square1_width, square1_y + square1_height, box_color_green);
+            }
+
+            // Verifique a segunda caixa (quadrado azul)
+            if (mouse_x >= square2_x && mouse_x <= square2_x + square2_width &&
+                mouse_y >= square2_y && mouse_y <= square2_y + square2_height) {
+                // O mouse está dentro da segunda caixa (quadrado azul)
+                al_draw_filled_rectangle(square2_x, square2_y, square2_x + square2_width, square2_y + square2_height, highlight_color);
+            }
+            else {
+                // O mouse não está dentro da segunda caixa (quadrado azul)
+                al_draw_filled_rectangle(square2_x, square2_y, square2_x + square2_width, square2_y + square2_height, box_color_blue);
+            }
+
             
             // -------------------------------------------------
             al_set_target_backbuffer(disp);
